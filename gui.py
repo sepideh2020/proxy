c832ffb42385bc12f3213fadb1ee66277d5280ac
sepideh2020 <sepideniktab2020@gmail.com>
2019-03-08 16:18:25 +0330

    almost finished :)

--- gui.py
+++ gui.py
@@ -9,64 +9,74 @@ import subprocess as sp
  
 window = Tk()
  
-window.title("filter site")
+window.title("Manage Sites")
  
 window.geometry('1000x1000')
 
-lbl = Label(window, text="add group :", font=30)
+window.configure(background="white")
+
+
+bg_image = PhotoImage(file ="images.png")
+
+x = Label (image = bg_image)
+
+x.grid(row = 0, column = 0)
+
+
+
+lbl = Label(window, text="Add group :", font=30,background="white")
  
-lbl.grid(column=1, row=1)
+lbl.grid(column=0, row=1)
  
 txt = Entry(window,width=20)
  
 txt.grid(column=3, row=1)
+
+
+def addGroup():
  
-def clicked():
- 
-    #res = txt.get()
     fileName = txt.get()
 
     if os.path.isfile(fileName)==True:
         messagebox.showwarning('Warning', 'File exists')
         exit
-        
-    #f=open("filtered.txt","a+")
-    #f=open(fileName,"a+")
+    
     filepath=os.path.join("file_directory",fileName)
     f = open(filepath, "a")
-    #f.write(res + '\n')
-    #print()
-    
+
     f.close
 
-btn = Button(window, text="ADD", command=clicked)
+btn = Button(window, text="ADD", command=addGroup)
 btn.grid(column=4, row=1)
 
+"""*********************************************************************"""
 
 combo = Combobox(window)
+combo.grid(column=3, row=2)
 
 for filenames in  os.walk("file_directory"):
     for file in filenames:
       combo['values']=file
 
-combo.grid(column=0, row=10)
 
 def openFile():
     fileName=combo.get()
 
     filepath=os.path.join("file_directory",fileName)
     print(filepath)
-    #file=open(filepath,"t")
+   
     programName="gedit"
     sp.Popen([programName,filepath])
     
     
+lb= Label(window, text="open group :",font=30,background="white")
 
-button = Button(window, text="ADD", command=openFile)
-button.grid(column=0, row=11)
+lb.grid(column=0, row=2)
 
+button = Button(window, text="OPEN", command=openFile)
 
-
+button.grid(column=4, row=2)
 
 window.mainloop()
+
 
