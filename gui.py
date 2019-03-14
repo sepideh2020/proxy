e8f049c24b2dc3bcbf17f11d51562b16daad1692
sepideh2020 <sepideniktab2020@gmail.com>
2019-03-14 10:03:07 +0330

    proxy doesnt work:((

--- gui.py
+++ gui.py
@@ -6,8 +6,10 @@ import os.path
 from tkinter.ttk import *
 from os import listdir
 import subprocess as sp
- 
-window = Tk()
+import tkinter as tk
+from math import *
+
+window = tk.Tk()
  
 window.title("Manage Sites")
  
@@ -18,7 +20,7 @@ window.configure(background="white")
 
 bg_image = PhotoImage(file ="images.png")
 
-x = Label (image = bg_image)
+x = Label (image = bg_image,borderwidth=0)
 
 x.grid(row = 0, column = 0)
 
@@ -26,11 +28,11 @@ x.grid(row = 0, column = 0)
 
 lbl = Label(window, text="Add group :", font=30,background="white")
  
-lbl.grid(column=0, row=1)
+lbl.grid(column=1, row=6)
  
 txt = Entry(window,width=20)
  
-txt.grid(column=3, row=1)
+txt.grid(column=3, row=6)
 
 
 def addGroup():
@@ -46,12 +48,15 @@ def addGroup():
     f.close
 
 btn = Button(window, text="ADD", command=addGroup)
-btn.grid(column=4, row=1)
+btn.grid(column=4, row=6)
 
 """*********************************************************************"""
+lb= Label(window, text="open group :",font=30,background="white",justify = 'center)
+
+lb.grid(column=1, row=8)
 
 combo = Combobox(window)
-combo.grid(column=3, row=2)
+combo.grid(column=3, row=8)
 
 for filenames in  os.walk("file_directory"):
     for file in filenames:
@@ -68,14 +73,45 @@ def openFile():
     sp.Popen([programName,filepath])
     
     
-lb= Label(window, text="open group :",font=30,background="white")
-
-lb.grid(column=0, row=2)
-
 button = Button(window, text="OPEN", command=openFile)
 
-button.grid(column=4, row=2)
+button.grid(column=4, row=8)
+
+
+
+"""**************************************************************************"""
+
+
+def choose():
+    # for value (1&2) just put in what you need for it
+    # for example, I just put a string '101' and '102' in value1 & 2
+    # the control variable cb(n)_v would hold 1 if its checked and 0 if not
+
+    result = []
+    if cb1_v == 1:
+        result.append(value1)
+    if cb2_v == 1:
+        result.append(value2)
+    return result
+
+
+i=0
+for file in  os.walk("file_directory"):
+        print(file)
+              
+        cb = tk.Checkbutton(window, text=file, variable=tk.IntVar())
+        cb.grid(row=0,column=i)
+        i=i+1
+        
+        
+        btn1=tk.Button(window, text='Choose',command=choose)
+        
+        
+
+btn1.grid(row=2, column=0)
+
 
 window.mainloop()
+
 
 
